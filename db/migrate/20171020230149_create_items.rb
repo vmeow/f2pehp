7338f3c50a12fbc1a9f1ac11d98f19a0bb6ae0ab
dsproch <dsproch@berkeley.edu>
2017-10-20 16:21:51 -0700

    created migration

--- db/migrate/20171020230149_create_items.rb
+++ db/migrate/20171020230149_create_items.rb
@@ -5,4 +5,22 @@ class CreateItems < ActiveRecord::Migration
       t.timestamps null: false
     end
   end
+
+
+  def up
+    create_table :items do |t|
+      t.string :client_name
+      t.string :client_ssn
+      t.string :case_type
+      t.string :status
+      t.datetime :date_opened
+      t.datetime :date_closed
+      
+      t.timestamps null: false
+    end
+  end
+  
+  def down
+    drop_table :items
+  end
 end
