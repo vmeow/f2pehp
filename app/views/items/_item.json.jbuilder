ee6dea5743bb74651083785a47c3fdd4dfe375a0
Ian McDonald <ian@airbears2-10-142-60-103.airbears2.1918.berkeley.edu>
2017-10-20 16:04:45 -0700

    generated scaffold in haml

---
+++ app/views/items/_item.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! item, :id, :created_at, :updated_at
+json.url item_url(item, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20171020230149) do
+
+  create_table "items", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
