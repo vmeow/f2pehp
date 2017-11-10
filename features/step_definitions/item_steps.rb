bc068e040aea5369ab7d402f11c1da000cee322b
dsproch <dsproch@berkeley.edu>
2017-11-10 15:22:24 -0800

    [test] updated feature file form_headings

--- features/step_definitions/item_steps.rb
+++ features/step_definitions/item_steps.rb
@@ -18,6 +18,14 @@ Then /^I should (not )?see "(.*)" before "(.*)"$/ do |not_see, e1, e2|
   end
 end
 
+Then /^I should (not )?see "(.*)" after "(.*)"$/ do |not_see, e1, e2|
+  if not_see
+    expect(page.body.index(e1)).to be < page.body.index(e2)
+  else
+    expect(page.body.index(e1)).to be > page.body.index(e2)
+  end
+end
+
 Then /^The client_ssn of item "(.*)" should be "(.*)"$/ do |item_id, val|
   item_attr = Item.find_by(item_id).client_ssn
   expect(item_attr.to eq(val))
