999b443cfb639aae6c778027f9c4bc033890fdf6
Victor Miao <vmiao1@berkeley.edu>
2017-11-12 06:28:28 +0000

    Update to v0.2.2a

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'players#index'
   get 'changelog' => 'players#changelog'
+  get 'ehp' => 'players#ehp'
   post 'players/:id' => 'players#update'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
