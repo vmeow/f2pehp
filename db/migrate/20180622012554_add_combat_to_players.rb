63c4eb06a4a8db93c88b9ff6efff351d6a99b494
Victor Miao <vmiao1@berkeley.edu>
2018-06-22 01:38:10 +0000

    [update] float combat lvl

--- db/migrate/20180622012554_add_combat_to_players.rb
+++ db/migrate/20180622012554_add_combat_to_players.rb
@@ -1,5 +1,5 @@
 class AddCombatToPlayers < ActiveRecord::Migration
   def change
-    add_column :players, :combat_lvl, :float
+    add_column :players, :combat_lvl, :int
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180622012554) do
+ActiveRecord::Schema.define(version: 20180622013652) do
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
