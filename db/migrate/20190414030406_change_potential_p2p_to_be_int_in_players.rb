012871b7163c6cd95ee62814e56ca67421b85128
Victor Miao <vmiao1@berkeley.edu>
2019-04-13 21:28:33 -0700

    [fix] migration, changelog

--- db/migrate/20190414030406_change_potential_p2p_to_be_int_in_players.rb
+++ db/migrate/20190414030406_change_potential_p2p_to_be_int_in_players.rb
@@ -1,5 +1,5 @@
 class ChangePotentialP2pToBeIntInPlayers < ActiveRecord::Migration
   def change
-    change_column :players, :potential_p2p, :int
+    change_column :players, :potential_p2p, 'integer USING CAST(potential_p2p AS integer)'
   end
 end
