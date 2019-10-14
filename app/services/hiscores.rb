011e9ebd72e78426be01939c47b211f7ca6c6c5b
Mike Voets <mwhg.voets@gmail.com>
2019-10-14 17:45:25 +1100

    Add method to check if HCIM is dead

--- app/services/hiscores.rb
+++ app/services/hiscores.rb
@@ -20,6 +20,13 @@ class Hiscores
       )
     end
 
+    def hcim_table_url(player_name)
+      URI.join(
+        'https://secure.runescape.com/m=hiscore_oldschool_hardcore_ironman/overall.ws',
+        "?user=#{player_name}"
+      )
+    end
+
     def parse_stats(data, restrict_fields = [])
       stats = { potential_p2p: 0 }
 
@@ -76,6 +83,14 @@ class Hiscores
       parse_stats(data, parse_fields)
     rescue OpenURI::HTTPError
       raise RuntimeError, "player #{player_name} is not a(n) #{account_type}"
+    end
+
+    def hcim_dead?(player_name)
+      uri = hcim_table_url(player_name)
+      page = Nokogiri::HTML(open(uri))
+      page.xpath('//*[@id="contentHiscores"]/table/tbody/tr[contains(@class, "--dead")]/td/a/span')
+          .first
+          .present?
     end
 
     # Checks if given `account_type` for `player_name` is still valid.
