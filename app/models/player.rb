1aed57a628143ee1674b81aa257270a1b8622461
puremana <puremana@gmail.com>
2020-05-05 21:14:36 +1200

    [SUPPORTERS] xxcxzx

--- app/models/player.rb
+++ app/models/player.rb
@@ -187,6 +187,7 @@ class Player < ActiveRecord::Base
                 {name: "TheNutSlush", amount: 5, date: "2020-04-19", flair_after: "flairs/Reward_casket_beginner.png"},
                 {name: "maddiefsna5", amount: 5, date: "2020-04-23", no_link: true},
                 {name: "Ultw", amount: 5, date: "2020-04-28"},
+                {name: "xxcxzx", amount: 5, date: "2020-05-05"},
                 {name: "Tohno1612", amount: ??, flair_after: "flairs/addy_helm.png"},
                 {name: "H C Gilrix", amount: 2.5, date: "2018-03-04", flair_after: "flairs/HCIM.png"},
                 {name: "Anonymous", amount: 2.5, date: "2018-07-26", no_link: true},
