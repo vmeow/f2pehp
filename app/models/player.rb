16f849097a4a9047a21b5b8fd80e10ab7c9735bc
puremana <puremana@gmail.com>
2020-12-30 13:13:49 +1300

    [SUPPORTERS] Add F2P Verf

--- app/models/player.rb
+++ app/models/player.rb
@@ -250,6 +250,7 @@ class Player < ActiveRecord::Base
                 {name: "DopeAssF2p", amount: 5, date: "2020-11-28", flair_after: "flairs/Amulet_of_power.png"},
                 {name: "Ironthokk", amount: 5, date: "2020-12-01", flair_after: "flairs/Maple_shortbow.png"},
                 {name: "Kev F2P", amount: 5, date: "2020-12-28", flair_after: "flairs/bunny_ears.png"},
+                {name: "F2P Verf", amount: 5, date: "2020-12-30", flair_after: "flairs/Dutch_flag.png"},
                 {name: "Tohno1612", amount: ??, flair_after: "flairs/addy_helm.png"},
                 {name: "H C Gilrix", amount: 2.5, date: "2018-03-04", flair_after: "flairs/HCIM.png"},
                 {name: "Anonymous", amount: 2.5, date: "2018-07-26", no_link: true},
