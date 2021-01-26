7fc71febf9b446d02d057dca2950fad474858d0a
puremana <puremana@gmail.com>
2021-01-27 07:36:56 +1300

    [SUPPORTERS] Add WeldingIM with estonia flag

--- app/models/player.rb
+++ app/models/player.rb
@@ -259,6 +259,7 @@ class Player < ActiveRecord::Base
                 {name: "mythiclime", amount: 5, date: "2021-01-10", flair_after: "items/Adamantite_bar.gif"},
                 {name: "Reviloekul", amount: 5, date: "2021-01-13", flair_after: "flairs/Black_hween_mask.png"},
                 {name: "SYNTHETlX", amount: 5, date: "2021-01-22", flair_after: "flairs/SYNTHETlX.png", other_css: ["color: #01D1FE"]},
+                {name: "WeldingIM", amount: 5, date: "2021-01-26", flair_after: "flairs/estonia_flag.png"},
                 {name: "Tohno1612", amount: ??, flair_after: "flairs/addy_helm.png"},
                 {name: "H C Gilrix", amount: 2.5, date: "2018-03-04", flair_after: "flairs/HCIM.png"},
                 {name: "Anonymous", amount: 2.5, date: "2018-07-26", no_link: true},
