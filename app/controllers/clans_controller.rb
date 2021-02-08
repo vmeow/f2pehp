44a7d2b7b3eac8c47074c906c7af496319339301
Victor Miao <vmiao1@berkeley.edu>
2021-02-08 00:57:08 -0800

    [DATA] tracking and records for clans

--- app/controllers/clans_controller.rb
+++ app/controllers/clans_controller.rb
@@ -150,7 +150,26 @@ class ClansController < ApplicationController
       @skill = "overall"
       params[:skill] = "overall"
       session[:skill] = "overall"
-    end 
+    end
+
+    @display = params[:display] || session[:display] || "stats"
+    @time = params[:time] || session[:time] || "week"
+
+    if params[:display] != session[:display] || params[:time] != session[:time]
+      session[:display] = @display
+      session[:time] = @time
+    end
+
+    case @time
+    when "day"
+      @time_display = Time.now.gmtime.strftime("%b %d, %Y")
+    when "week"
+      @time_display = Time.now.gmtime.beginning_of_week.strftime("%b %d, %Y")
+    when "month"
+      @time_display = Time.now.gmtime.strftime("%b %Y")
+    when "year"
+      @time_display = Time.now.gmtime.year
+    end
 
     if params[:sort_by] != session[:sort_by] || params[:skill] != session[:skill]
       session[:skill] = @skill
@@ -165,52 +184,86 @@ class ClansController < ApplicationController
       @sort_by = "ehp"
     end
 
-    if @skill.include?("ttm")
-      case @skill
-      when "ttm_lvl"
-        ordering = "ttm_lvl ASC, overall_ehp DESC"
-      when "ttm_xp"
-        ordering = "ttm_xp ASC, overall_ehp DESC"
-      end
-    elsif @skill.include?("clues")
-      case @skill
-      when "clues_all"
-        ordering = "clues_all DESC, id ASC"
-      when "clues_beginner"
-        ordering = "clues_beginner DESC, id ASC"
-      end
-    elsif @skill.include?("no_combats")
-      case @sort_by
-      when "ehp"
-        ordering = "fishing_ehp + cooking_ehp + woodcutting_ehp + firemaking_ehp + mining_ehp + smithing_ehp + crafting_ehp + runecraft_ehp DESC"
-      when "lvl"
-        ordering = "fishing_lvl + cooking_lvl + woodcutting_lvl + firemaking_lvl + mining_lvl + smithing_lvl + crafting_lvl + runecraft_lvl DESC, fishing_ehp + cooking_ehp + woodcutting_ehp + firemaking_ehp + mining_ehp + smithing_ehp + crafting_ehp + runecraft_ehp DESC, fishing_xp + cooking_xp + woodcutting_xp + firemaking_xp + mining_xp + smithing_xp + crafting_xp + runecraft_xp DESC"
-      when "xp"
-        ordering = "fishing_xp + cooking_xp + woodcutting_xp + firemaking_xp + mining_xp + smithing_xp + crafting_xp + runecraft_xp DESC, fishing_ehp + cooking_ehp + woodcutting_ehp + firemaking_ehp + mining_ehp + smithing_ehp + crafting_ehp + runecraft_ehp DESC, fishing_lvl + cooking_lvl + woodcutting_lvl + firemaking_lvl + mining_lvl + smithing_lvl + crafting_lvl + runecraft_lvl DESC"
-      end
-    elsif @skill.include?("count")
-      ordering = "overall_ehp DESC"
-    elsif @skill.include?("lowest_lvl")
-      ordering = "overall_ehp DESC"
-    elsif @skill.include?("lms")
-      ordering = "lms_score DESC, lms_rank ASC"
-    elsif @skill.include?("_kc")
-      ordering = "#{@skill} DESC, #{@skill}_rank ASC"
-    else
-      case @sort_by
-      when "ehp"
-        @player_ehp_header = 'hilite'
-        ordering = "#{@skill}_ehp DESC, #{@skill}_lvl DESC, #{@skill}_xp DESC, #{@skill}_rank ASC, id ASC"
-      when "lvl"
-        @player_lvl_header = 'hilite'
-        if @skill == "combat"
-          ordering = "#{@skill}_lvl DESC, overall_ehp DESC"
-        else
-          ordering = "#{@skill}_lvl DESC, #{@skill}_xp DESC, #{@skill}_rank ASC"
+    if @display == "stats"
+      if @skill.include?("ttm")
+        case @skill
+        when "ttm_lvl"
+          ordering = "ttm_lvl ASC, overall_ehp DESC"
+        when "ttm_xp"
+          ordering = "ttm_xp ASC, overall_ehp DESC"
         end
+      elsif @skill.include?("clues")
+        case @skill
+        when "clues_all"
+          ordering = "clues_all DESC, id ASC"
+        when "clues_beginner"
+          ordering = "clues_beginner DESC, id ASC"
+        end
+      elsif @skill.include?("no_combats")
+        case @sort_by
+        when "ehp"
+          ordering = "fishing_ehp + cooking_ehp + woodcutting_ehp + firemaking_ehp + mining_ehp + smithing_ehp + crafting_ehp + runecraft_ehp DESC"
+        when "lvl"
+          ordering = "fishing_lvl + cooking_lvl + woodcutting_lvl + firemaking_lvl + mining_lvl + smithing_lvl + crafting_lvl + runecraft_lvl DESC, fishing_ehp + cooking_ehp + woodcutting_ehp + firemaking_ehp + mining_ehp + smithing_ehp + crafting_ehp + runecraft_ehp DESC, fishing_xp + cooking_xp + woodcutting_xp + firemaking_xp + mining_xp + smithing_xp + crafting_xp + runecraft_xp DESC"
+        when "xp"
+          ordering = "fishing_xp + cooking_xp + woodcutting_xp + firemaking_xp + mining_xp + smithing_xp + crafting_xp + runecraft_xp DESC, fishing_ehp + cooking_ehp + woodcutting_ehp + firemaking_ehp + mining_ehp + smithing_ehp + crafting_ehp + runecraft_ehp DESC, fishing_lvl + cooking_lvl + woodcutting_lvl + firemaking_lvl + mining_lvl + smithing_lvl + crafting_lvl + runecraft_lvl DESC"
+        end
+      elsif @skill.include?("count")
+        ordering = "overall_ehp DESC"
+      elsif @skill.include?("lowest_lvl")
+        ordering = "overall_ehp DESC"
+      elsif @skill.include?("lms")
+        ordering = "lms_score DESC, lms_rank ASC"
+      elsif @skill.include?("_kc")
+        ordering = "#{@skill} DESC, #{@skill}_rank ASC"
+      else
+        case @sort_by
+        when "ehp"
+          @player_ehp_header = 'hilite'
+          ordering = "#{@skill}_ehp DESC, #{@skill}_lvl DESC, #{@skill}_xp DESC, #{@skill}_rank ASC, id ASC"
+        when "lvl"
+          @player_lvl_header = 'hilite'
+          if @skill == "combat"
+            ordering = "#{@skill}_lvl DESC, overall_ehp DESC"
+          else
+            ordering = "#{@skill}_lvl DESC, #{@skill}_xp DESC, #{@skill}_rank ASC"
+          end
+        when "xp"
+          @player_xp_header = 'hilite'
+          ordering = "#{@skill}_xp DESC, #{@skill}_rank ASC"
+        end
+      end
+    elsif @display == "gains"
+      unless F2POSRSRanks::Application.config.f2p_skills.include?(@skill)
+        @skill = "overall"
+        params[:skill] = "overall"
+        session[:skill] = "overall"
+      end
+
+      case @sort_by
+      when "ehp", "lvl"
+        @sort_by = "ehp"
+        @player_ehp_header = 'hilite'
+        ordering = "#{@skill}_ehp - #{@skill}_ehp_#{@time}_start DESC, #{@skill}_xp - #{@skill}_xp_#{@time}_start DESC, #{@skill}_ehp DESC, #{@skill}_xp DESC, id ASC"
       when "xp"
         @player_xp_header = 'hilite'
-        ordering = "#{@skill}_xp DESC, #{@skill}_rank ASC"
+        ordering = "#{@skill}_xp - #{@skill}_xp_#{@time}_start DESC, #{@skill}_ehp - #{@skill}_ehp_#{@time}_start DESC, #{@skill}_xp DESC"
+      end
+    elsif @display == "records"
+      unless F2POSRSRanks::Application.config.f2p_skills.include?(@skill)
+        @skill = "overall"
+        params[:skill] = "overall"
+        session[:skill] = "overall"
+      end
+
+      case @sort_by
+      when "ehp", "lvl"
+        @sort_by = "ehp"
+        @player_ehp_header = 'hilite'
+        ordering = "#{@skill}_ehp_#{@time}_max DESC, #{@skill}_xp_#{@time}_max DESC, #{@skill}_ehp DESC, #{@skill}_xp DESC, id ASC"
+      when "xp"
+        @player_xp_header = 'hilite'
+        ordering = "#{@skill}_xp_#{@time}_max DESC, #{@skill}_ehp_#{@time}_max DESC, #{@skill}_xp DESC"
       end
     end
 
