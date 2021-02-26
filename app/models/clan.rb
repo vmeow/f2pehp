9b4d01e16f7cb46a34af862392bb7fcdb161382a
Victor Miao <vmiao1@berkeley.edu>
2021-02-25 19:42:40 -0800

    [DATA] add some comments for adding clans. 'Bump' version, and create new entry on front page. Re-name 'versions' to be stnadard major.minor.patch format

--- app/models/clan.rb
+++ app/models/clan.rb
@@ -1,4 +1,9 @@
 class Clan < ActiveRecord::Base
+  # This model intentionally does not have a way of conveniently adding records.
+  # Due to the Heroku 10000 row limit, we need to limit the number of clans, players, and clan links.
+  # In order to create a new clan, run the following in the Rails console:
+  #    `Clan.create!({:name => "Some Clan", :symbol_link => "some_clan.png", :pass => Digest::MD5.hexdigest("mypassword")})`
+
   has_many :player_clan_links
   has_many :players, through: :player_clan_links
 
