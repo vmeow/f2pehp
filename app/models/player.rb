2ad1323df032ce56d9e43358a723049c6ce60d0c
puremana <puremana@gmail.com>
2021-03-27 21:47:47 +1300

    [SUPPORTERS] Add Rykkirs

--- app/models/player.rb
+++ app/models/player.rb
@@ -281,6 +281,7 @@ class Player < ActiveRecord::Base
                 {name: "wytchblades", amount: 5, date: "2021-03-08", flair_after: "flairs/Red_halloween_mask.png"},
                 {name: "Xxjx", amount: 5, date: "2021-03-21", flair_after: "flairs/rune_med_helm.png"},
                 {name: "boneyghost", amount: 5, date: "2021-03-25", flair_after: "flairs/Bones.png"},
+                {name: "Rykkirs", amount: 5, date: "2021-03-26", flair_after: "flairs/Inverted_santa_hat.png"},
                 {name: "Tohno1612", amount: ??, flair_after: "flairs/addy_helm.png"},
                 {name: "H C Gilrix", amount: 2.5, date: "2018-03-04", flair_after: "flairs/HCIM.png"},
                 {name: "Anonymous", amount: 2.5, date: "2018-07-26", no_link: true},
