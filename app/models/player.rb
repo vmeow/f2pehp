f711d4524561abf56c8237d2bb14b39d7c17f866
Victor Miao <vmiao@valorwater.com>
2021-06-02 12:32:19 -0700

    [SUPPORTERS] Fix capitalization of F2PUIM Mkoll. [LINKS] add boneyghost

--- app/models/player.rb
+++ app/models/player.rb
@@ -295,7 +295,7 @@ class Player < ActiveRecord::Base
                 {name: "IownFarah", amount: 5, date: "2021-05-22"},
                 {name: "Twink Skills", amount: 5, date: "2021-05-27", flair_after: "flairs/Strength_amulet_t.png"},
                 {name: "reubengonz", amount: 5, date: "2021-05-30", flair_after: "flairs/Pet_cat_grey_and_blue.png"},
-                {name: "F2PUIM MKoll", amount: 5, date: "2021-05-31", flair_after: "flairs/Killers_knife.png"},
+                {name: "F2PUIM Mkoll", amount: 5, date: "2021-05-31", flair_after: "flairs/Killers_knife.png"},
                 {name: "Zaadkameraad", amount: 5, date: "2021-06-02", flair_after: "flairs/white_pick.png", other_css: ["color: white"]},
                 {name: "Tohno1612", amount: ??, flair_after: "flairs/addy_helm.png"},
                 {name: "H C Gilrix", amount: 2.5, date: "2018-03-04", flair_after: "flairs/HCIM.png"},
