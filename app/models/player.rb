f410860a660ccf2c784fd8eba18fbe5531fc9215
puremana <puremana@gmail.com>
2021-07-13 09:27:20 +1200

    [SUPPORTERS] Add MoleSlippers

--- app/models/player.rb
+++ app/models/player.rb
@@ -302,6 +302,7 @@ class Player < ActiveRecord::Base
                 {name: "Zaadkameraad", amount: 5, date: "2021-06-02", flair_after: "flairs/white_pick.png", other_css: ["color: white"]},
                 {name: "i Picard", amount: 5, date: "2021-06-07", flair_after: "flairs/i_Picard.png"},
                 {name: "TexWasabi", amount: 5, date: "2021-07-04"},
+                {name: "MoleSlippers", amount: 5, date: "2021-07-12", flair_after: "flairs/Mole_slippers.png"},
                 {name: "Tohno1612", amount: ??, flair_after: "flairs/addy_helm.png"},
                 {name: "H C Gilrix", amount: 2.5, date: "2018-03-04", flair_after: "flairs/HCIM.png"},
                 {name: "Anonymous", amount: 2.5, date: "2018-07-26", no_link: true},
