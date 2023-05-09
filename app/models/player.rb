7c24d21c40785a012c1dce222b2871506b023bac
puremana <puremana@gmail.com>
2023-05-09 17:13:14 +1200

    [SUPPORTERS] Add Defbone

--- app/models/player.rb
+++ app/models/player.rb
@@ -187,6 +187,7 @@ class Player < ActiveRecord::Base
                   {name: "gluee9", amount: 10, date: "2022-11-07", flair_after: "flairs/S.png"},
                 {name: "Held Items", amount: 10, date: "2022-11-15", flair_after: "flairs/leek.png"},
                 {name: "Crazy Gadget", amount: 10, date: "2023-01-20", flair_after: "flairs/sonic.png"},
+                {name: "Defbone", amount: 10, date: "2023-05-09", flair_after: "flairs/cwars_white_plate.png"},
                 {name: "n0neshine", amount: 5, date: "2021-06-12", flair_after: "flairs/n0neshine.png"},
                 {name: "Swiss Corona", amount: 8, date: "2020-11-06", flair_after: "flairs/switzerland_flag.png", other_css: ["color: #FF0000"]},
                 {name: "For Ulven", amount: 7.77, date: "2018-03-11", flair_after: "flairs/wolf.png"},
